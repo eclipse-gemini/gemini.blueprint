77a8196b7bb3733197587dee3c0bf12da22d45da
Aaron Whiteside <aaronjwhiteside@gmail.com>
2012-04-14 19:41:01 -0400

    Support for junit 4.7+ maven test runner, previously this test would not have been run and without this change it receives a No runnable methods found error.

--- core/src/test/java/org/eclipse/gemini/blueprint/internal/service/collection/threading/BaseThreadingTest.java
+++ core/src/test/java/org/eclipse/gemini/blueprint/internal/service/collection/threading/BaseThreadingTest.java
@@ -16,21 +16,17 @@ package org.eclipse.gemini.blueprint.internal.service.collection.threading;
 
 import edu.umd.cs.mtc.MultithreadedTest;
 import edu.umd.cs.mtc.TestFramework;
+import org.junit.Test;
 
 /**
  * @author Costin Leau
  * 
  */
 public abstract class BaseThreadingTest extends MultithreadedTest {
-
 	static int RUN_TIMES = 3;
 
+    @Test
 	public void runTest() throws Throwable {
 		TestFramework.runManyTimes(this, RUN_TIMES);
 	}
-
-	public void testRun() throws Throwable {
-		// do nothing - just to comply with the test suite
-	}
-
 }
