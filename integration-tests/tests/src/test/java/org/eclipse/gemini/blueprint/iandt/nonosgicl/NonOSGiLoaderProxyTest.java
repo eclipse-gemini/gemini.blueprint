f44d85645024aab34071f138941864ce4ca05ddd
Dmitry Sklyut <dmitry@dsklyut.com>
2013-10-25 11:25:58 -0400

    377327 build fails on jdk 7

--- integration-tests/tests/src/test/java/org/eclipse/gemini/blueprint/iandt/nonosgicl/NonOSGiLoaderProxyTest.java
+++ integration-tests/tests/src/test/java/org/eclipse/gemini/blueprint/iandt/nonosgicl/NonOSGiLoaderProxyTest.java
@@ -17,6 +17,7 @@ package org.eclipse.gemini.blueprint.iandt.nonosgicl;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Comparator;
 import java.util.Hashtable;
 import java.util.logging.Logger;
@@ -77,7 +78,7 @@ public class NonOSGiLoaderProxyTest extends BaseIntegrationTest {
 			return null;
 		}
 
-        public Logger getParentLogger() throws SQLException {
+        public Logger getParentLogger() throws SQLFeatureNotSupportedException {
             return null;
         }
     }
