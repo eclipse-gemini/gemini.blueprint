160df3dee506efe06a14ad9eb70492cbb21a41e9
fantasy1713 <fantasy1713@qq.com>
2022-11-24 14:59:49 +0800

    The Spring-Context configuration supports environment variables

--- extender/src/main/java/org/eclipse/gemini/blueprint/extender/support/scanning/DefaultConfigurationScanner.java
+++ extender/src/main/java/org/eclipse/gemini/blueprint/extender/support/scanning/DefaultConfigurationScanner.java
@@ -7,19 +7,21 @@
  * http://www.eclipse.org/legal/epl-v10.html and the Apache License v2.0
  * is available at http://www.opensource.org/licenses/apache2.0.php.
  * You may elect to redistribute this code under either of these licenses. 
- * 
+ *
  * Contributors:
  *   VMware Inc.
  *****************************************************************************/
 
 package org.eclipse.gemini.blueprint.extender.support.scanning;
 
-import java.util.Enumeration;
-
 import org.eclipse.gemini.blueprint.extender.support.internal.ConfigUtils;
 import org.eclipse.gemini.blueprint.io.OsgiBundleResource;
 import org.osgi.framework.Bundle;
 import org.springframework.util.ObjectUtils;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Default implementation of {@link ConfigurationScanner} interface.
@@ -28,7 +30,7 @@ import org.springframework.util.ObjectUtils;
  * <tt>META-INF/spring/*.xml</tt>.
  * 
  * @author Costin Leau
- * 
+ *
  */
 public class DefaultConfigurationScanner implements ConfigurationScanner {
 
@@ -43,19 +45,41 @@ public class DefaultConfigurationScanner implements ConfigurationScanner {
 	public String[] getConfigurations(Bundle bundle) {
 		String[] locations = ConfigUtils.getHeaderLocations(bundle.getHeaders());
 
-		// if no location is specified in the header, try the defaults
-		if (ObjectUtils.isEmpty(locations)) {
-			// check the default locations if the manifest doesn't provide any info
-			Enumeration defaultConfig = bundle.findEntries(CONTEXT_DIR, CONTEXT_FILES, false);
-			if (defaultConfig != null && defaultConfig.hasMoreElements()) {
-				return new String[] { DEFAULT_CONFIG };
-			}
-			else {
-				return new String[0];
-			}
-		}
-		else {
-			return locations;
-		}
-	}
+        // if no location is specified in the header, try the defaults
+        if (ObjectUtils.isEmpty(locations)) {
+            // check the default locations if the manifest doesn't provide any info
+            Enumeration defaultConfig = bundle.findEntries(CONTEXT_DIR, CONTEXT_FILES, false);
+            if (defaultConfig != null && defaultConfig.hasMoreElements()) {
+                return new String[]{DEFAULT_CONFIG};
+            } else {
+                return new String[0];
+            }
+        } else {
+            return parseLocations(locations);
+        }
+    }
+
+    private String[] parseLocations(String... locations) {
+        List<String> parsedLocations = new ArrayList<>();
+        for (String location : locations) {
+            String parsed = replaceSysEnv(location);
+            parsedLocations.add(parsed);
+        }
+        return parsedLocations.toArray(new String[0]);
+    }
+
+    private String replaceSysEnv(String location) {
+        if (location != null && !location.isEmpty()) {
+            int envBegin = location.indexOf("${");
+            int envEnd = location.indexOf("}");
+            if (envBegin >= 0 && envEnd > envBegin) {
+                String envName = location.substring(envBegin+2, envEnd);
+                String envValue = System.getenv(envName);
+                if (envValue != null) {
+                    return "file:" + location.replace("${" + envName + "}", envValue);
+                }
+            }
+        }
+        return location;
+    }
 }
